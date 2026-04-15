977c2f223637f657dadffbb1f72e2e89310505e6
Hemantyadav17 <228730197+Hemantyadav17@users.noreply.github.com>
2026-04-15 10:00:20 +0530

    done  day 74

---
+++ Q147.c
@@ -0,0 +1,58 @@
+//Problem Statement
+//Given an array of candidate names where each name represents a vote cast for that candidate, determine the candidate who received the maximum number of votes. 
+//In case of a tie, return the lexicographically smallest candidate name.
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 1000
+#define LEN 50
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    char names[MAX][LEN];
+    int count[MAX] = {0};
+    int unique = 0;
+
+    for (int i = 0; i < n; i++) {
+        char temp[LEN];
+        scanf("%s", temp);
+
+        int found = -1;
+
+        for (int j = 0; j < unique; j++) {
+            if (strcmp(names[j], temp) == 0) {
+                found = j;
+                break;
+            }
+        }
+
+        if (found != -1) {
+            count[found]++;
+        } else {
+            strcpy(names[unique], temp);
+            count[unique] = 1;
+            unique++;
+        }
+    }
+
+    int maxVotes = 0;
+    char winner[LEN] = "";
+
+    for (int i = 0; i < unique; i++) {
+        if (count[i] > maxVotes) {
+            maxVotes = count[i];
+            strcpy(winner, names[i]);
+        } else if (count[i] == maxVotes) {
+            if (strcmp(names[i], winner) < 0) {
+                strcpy(winner, names[i]);
+            }
+        }
+    }
+
+    printf("%s %d\n", winner, maxVotes);
+
+    return 0;
+}
